a2d57455783b3507deb8cfaab186756791ddaee2
Ewa Kubik <ewa.kubik@hotmail.com>
2022-02-28 17:40:25 +1100

    set up html routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //import modules
-const fs = require('fs');
-const express = require('express');
-const path = ('path');
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
 //select port
 const PORT = process.env.PORT || 3004;
@@ -9,10 +9,26 @@ const app = express();
 
 //middleware to read data properly
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //serve static files from public folder
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+//HTML routes serving static files
+
+//get notes html page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+//get home page
+//had to move this below, '/notes/ routes, as otherwise index.html was preferentially returned
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html')))
+;
+
 
 //code executed if running successfully
-app.listen(PORT, () => console.log (`Now listening at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
